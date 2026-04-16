7243f64b7bbb737c6d419fbe3dddc4edbafb68dd
raktim-basic <ruma05518@gmail.com>
2026-04-16 17:40:30 +0530

    Update build.gradle.kts

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -35,18 +35,17 @@ compose.desktop {
         mainClass = "com.audiobasics.desk.MainKt"
 
         nativeDistributions {
-            targetFormats(TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "AudiobasicsDesk"
-            packageVersion = "1.0.0"
-            description = "Audiobasics Desktop Companion"
-            vendor = "raktim-basic"
+    targetFormats(TargetFormat.Msi, TargetFormat.Deb)
+    packageName = "AudiobasicsDesk"
+    packageVersion = "1.0.0"
+    description = "Audiobasics Desktop Companion"
+    vendor = "Raktim"
+    includeAllModules = true
 
-            windows {
-                menuGroup = "Audiobasics"
-                shortcut = true
-                dirChooser = true
-                upgradeUuid = "a8f3b2c1-4d7e-4a9f-b6c2-1e3d5f7a9b0c"
-            }
-        }
+    windows {
+        menuGroup = "Audiobasics"
+        shortcut = true
+        dirChooser = true
+        upgradeUuid = "a8f3b2c1-4d7e-4a9f-b6c2-1e3d5f7a9b0c"
     }
 }
