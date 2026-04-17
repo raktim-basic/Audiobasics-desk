759da26f882e278ac03e0b6bc3a3a7691a1df5db
raktim-basic <ruma05518@gmail.com>
2026-04-17 12:03:25 +0530

    Update build.gradle.kts

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -42,13 +42,13 @@ compose.desktop {
             packageVersion = "1.0.0"
             description = "Audiobasics Desktop Companion"
             vendor = "raktim-basic"
-            includeAllModules = true
 
             windows {
                 menuGroup = "Audiobasics"
                 shortcut = true
                 dirChooser = true
                 upgradeUuid = "a8f3b2c1-4d7e-4a9f-b6c2-1e3d5f7a9b0c"
+                javaHome = System.getenv("JAVA_HOME") ?: ""
             }
         }
     }
