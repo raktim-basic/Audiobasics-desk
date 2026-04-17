f81806d1589a366adb6127600d96b81d80b099df
raktim-basic <ruma05518@gmail.com>
2026-04-17 11:44:04 +0530

    Update Main.kt

--- composeApp/src/desktopMain/Main.kt
+++ composeApp/src/desktopMain/Main.kt
@@ -4,22 +4,14 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
-import java.io.File
 
-fun main() {
-    try {
-        application {
-            val state = rememberWindowState(width = 1100.dp, height = 720.dp)
-            Window(
-                onCloseRequest = ::exitApplication,
-                title = "Audiobasics Desk",
-                state = state
-            ) {
-                App()
-            }
-        }
-    } catch (e: Throwable) {
-        File(System.getProperty("user.home") + "/audiobasics-desk-crash.txt")
-            .writeText(e.stackTraceToString())
+fun main() = application {
+    val state = rememberWindowState(width = 800.dp, height = 600.dp)
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Audiobasics Desk",
+        state = state
+    ) {
+        App()
     }
 }
